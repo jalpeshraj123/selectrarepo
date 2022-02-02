0538853349d0231acaba61d8c7f40e26a2a8d740
Jalpesh <jalpeshraj123@gmail.com>
2022-02-02 10:17:49 +0530

    change in settings.php

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -769,14 +769,5 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
-$databases['default']['default'] = array (
-  'database' => '',
-  'username' => '',
-  'password' => '',
-  'prefix' => '',
-  'host' => '',
-  'port' => '3306',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-);
+
 $settings['config_sync_directory'] = 'sites/default/files/config_d0ekfUvFHFbGkcL57ZiB1CgfG-6KIAYYCmwW4VmadEKWijwMOfmKeephT9ckNPlYyboWnpPsgw/sync';
